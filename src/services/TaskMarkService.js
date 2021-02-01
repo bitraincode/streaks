c07e2e87568d39ee8b429092719302863f70b463
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-02-02 00:18:24 +0600

    Refactor TaskMarkService

--- src/services/TaskMarkService.js
+++ src/services/TaskMarkService.js
@@ -2,33 +2,50 @@ const {ServiceError} = require("../error"),
     { Task } = require("../models"),
     { TaskMark } = require("../models");
 
-const TaskMarkService = {
-    createMark: async (taskId) => {
-        const lastMark = await TaskMark.findAndCountAll({
-            where: {
-                taskId
-            }
-        }).then(res => {
-            return res.rows[res.count-1]
+userCheck = async (username, taskId) => {
+    return await Task.findByPk(taskId)
+        .then(task => {
+            return task.ownerUsername === username
         })
+}
 
-        if (new Date().toLocaleDateString() === lastMark.createdAt.toLocaleDateString() ) {
-            throw new ServiceError(400, "You have already marked this task today")
+const TaskMarkService = {
+    createMark: async (taskId, username, done) => {
+        if (!await userCheck(username, taskId)) {
+            throw new ServiceError(403, "Forbidden error")
         }
-        await TaskMark.create({taskId})
+        await TaskMark.create({taskId, done})
+
+        // if (new Date().toLocaleDateString() === lastMark.createdAt.toLocaleDateString() ) {
+        //     throw new ServiceError(400, "You have already marked this task today")
+        // }
+        // const lastMark = await TaskMark.findAndCountAll({
+        //     where: {
+        //         taskId
+        //     }
+        // }).then(res => {
+        //     return res.rows[res.count-1]
+        // })
     },
     getMarksByTaskId: async (username, id) => {
-        return await Task.findByPk(id)
-                .then( async task => {
-                    if (task.ownerUsername === username) {
-                        return await TaskMark.findAll({
-                            where: {
-                                taskId: id
-                            }
-                        })
-                    }
-                    throw new ServiceError(403,"Forbidden error")
-                })
+        if (await userCheck(username, id)) {
+            return await TaskMark.findAll({
+                where: {
+                    taskId: id
+                }
+            })
+        }
+        throw new ServiceError(403,"Forbidden error")
+    },
+    updateMark: async (taskId, markId, username, isDone) => {
+        if (! await userCheck(username, taskId)) {
+            throw new ServiceError(403, "Forbidden error")
+        }
+        await TaskMark.findByPk(markId)
+            .then(async mark => {
+                mark.done = isDone
+                await mark.save()
+            })
     },
 }
 
